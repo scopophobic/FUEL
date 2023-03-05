f0a2d8f750f95c0ac505c2daddcdc81645aec817
scopophobic <adithyanmadhu1234@hmail.com>
2023-03-05 18:43:00 +0530

    basic setup and navbar kinda done

---
+++ fuel/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MYnavbar from './components/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <MYnavbar />
+      
+    </div>
+
+  );
+}
+
+export default App;
